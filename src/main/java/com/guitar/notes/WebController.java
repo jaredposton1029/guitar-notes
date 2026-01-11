9483321952a7013cfc047df0d7b960ba686e3b6a
Jared Poston <jaredposton1029@gmail.com>
2026-01-11 13:34:50 -0500

    Add simple page navigation
    Also remove "greeting" web page

--- src/main/java/com/guitar/notes/WebController.java
+++ src/main/java/com/guitar/notes/WebController.java
@@ -15,13 +15,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class WebController {
 
-  @GetMapping("/greeting")
-  public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
-      Model model) {
-    model.addAttribute("name", name);
-    return "greeting";
-  }
-
   @GetMapping("/6-string-guitar")
   public String guitar(Model model) {
     List<String> strings = Arrays.asList("low E", "A", "D", "G", "B", "high E");
